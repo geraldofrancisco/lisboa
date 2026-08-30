312a61d0caabe2899d87856271230decb33cef2e
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2026-08-30 01:19:47 -0300

    commit inicial

---
+++ src/main/java/com/thor/lisboa/LisboaApplication.java
@@ -0,0 +1,13 @@
+package com.thor.lisboa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisboaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisboaApplication.class, args);
+	}
+
+}
